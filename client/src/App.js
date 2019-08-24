68920be4ac4fee605d4b76f0a655799c993b8cb8
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-08-23 23:38:40 -0400

    initial commit, changed favicona nd added placeholder for app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Placeholder from './Placeholder.png'
+
+function App() {
+  return (
+    <div className="App">
+    <img src={Placeholder} alt='placeholder logo' />
+
+    </div>
+  );
+}
+
+export default App;
