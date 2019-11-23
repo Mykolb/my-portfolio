6d6c1f9fd238ba392bfca82e701a2f2ac9b66a09
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-11-22 23:47:00 -0500

    created mobile view

--- src/about/ProfilePic.js
+++ src/about/ProfilePic.js
@@ -7,63 +7,62 @@ import '../styles/About.scss'
 
 
 
-    const useStyles = makeStyles(theme => ({
-        entireContainer: {
-          display: 'grid',
-          gridTemplateColumns: 'repeat(2, 1fr)',
-          gridGap: theme.spacing(1),
-          margin: '5% 4% 0% 4%',
-        },
-        aboutContainer: {
-          margin: '0 auto',
-          justifyContent: 'center',
-          border: '5px solid gold',
-          width: '100%',
-        },
-        aboutHeading: {
-          textAlign: 'center',
-        },
-        bigAvatar: {
-          width: 300,
-          height: 300,
-          margin: '10%',
-          borderRadius: '4%'
-        },
-        bioContainer: {
-          border: '4px solid brown',
-          width: '100%',
-          paddingLeft: '2%'
-        },
-        bio: {
-          textAlign: 'center',
-          width: '100%',
-        }
-      }));
+    // const useStyles = makeStyles(theme => ({
+    //     entireContainer: {
+    //       display: 'grid',
+    //       gridTemplateColumns: 'repeat(2, 1fr)',
+    //       gridGap: theme.spacing(1),
+    //       margin: '5% 4% 0% 4%',
+    //     },
+    //     aboutContainer: {
+    //       margin: '0 auto',
+    //       justifyContent: 'center',
+    //       border: '5px solid gold',
+    //       width: '100%',
+    //     },
+    //     aboutHeading: {
+    //       textAlign: 'center',
+    //     },
+    //     bigAvatar: {
+    //       width: 300,
+    //       height: 300,
+    //       margin: '10%',
+    //       borderRadius: '4%'
+    //     },
+    //     bioContainer: {
+    //       border: '4px solid brown',
+    //       width: '100%',
+    //       paddingLeft: '2%'
+    //     },
+    //     bio: {
+    //       textAlign: 'center',
+    //       width: '100%',
+    //     }
+    //   }));
       
       export default function ProfilePic() {
-        const classes = useStyles();
+        // const classes = useStyles();
 
 
     return(
       <div>
-        <div className={classes.aboutContainer}>
-        <h2 className={classes.aboutHeading}>ABOUT</h2>
+        <div className='aboutContainer'>
+        <h2 className='aboutHeading' style={{ textAlign: 'center'}}>ABOUT</h2>
         </div>
-        <div className={classes.entireContainer}>
+        <div className='entireContainer'>
         <Grid container direction='row' justify="center" alignItems="baseline" item xs={6}>
         <Avatar 
         alt="profile picture" 
         src={Profile} 
-        className={classes.bigAvatar} 
+        className='bigAvatar' 
         />
       </Grid>
-      <div className={classes.bioContainer}>
+      <div className='bioContainer'>
         <Grid container direction='row' justify="center" alignItems="baseline" item xs={6}>
-        <p className={classes.bio}>Some fasinating info about me goes here. Some fasinating info about me goes here. 
-          Some fasinating info about me goes here. Some fasinating info about me goes here. Some fasinating info about me goes here.
-          Some fasinating info about me goes here. Some fasinating info about me goes here. Some fasinating info about me goes here. 
-          Some fasinating info about me goes here. Some fasinating info about me goes here. Some fasinating info about me goes here. 
-          Some fasinating info about me goes here. Some fasinating info about me goes here. Some fasinating info about me goes here. 
+        <p className='bio'>
+          Some fasinating info about me goes here. Some fasinating info about me goes here. 
+          Some fasinating info about me goes here. Some fasinating info about me goes here.  
+          Some fasinating info about me goes here. Some fasinating info about me goes here.  
        </p>
        </Grid>
       </div>
