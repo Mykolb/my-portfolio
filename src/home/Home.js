7b6765c43071bd4f14f26955a6da2d2a8f1a87e9
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-12-31 22:29:45 -0500

    made Particle Animation into reuseable component

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 import Particles from 'react-particles-js';
 import { withRouter } from 'react-router-dom';
 import '../styles/Home.scss';
+import ParticleAnimation from '../particle_animation/ParticleAnimation'
 
 const Home = () => {
 
-//created variable for sound clip 
+// created variable for sound clip 
 let audio = new Audio('https://media.merriam-webster.com/soundc11/bix/bixmic01.wav')
 
 const soundEffect = () => {
@@ -15,71 +16,7 @@ const soundEffect = () => {
 return(
 	<>
     <div id='home-section' className='welcome-div' > 
-       <Particles
-	className='design-background'
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 160,
-	            "density": {
-	                "enable": false,
-	            }
-			},
-			"shape": {
-			"type": "circle",
-			"stroke": {
-				"width": 2,
-				"color": '#B3A190',
-			}
-			},
-			"color": {
-				"value": [ '#E63629', '#EBB604', '#008080' ]
-			},
-	        "size": {
-	            "value": 5,
-	            "random": true,
-	            "anim": {
-	                "speed": 1,
-	                "size_min": 0.3
-	            }
-	        },
-	        "line_linked": {
-	            "enable": false
-	        },
-	        "move": {
-	            "random": true,
-	            "speed": 3,
-	            "direction": "top",
-	            "out_mode": "bounce"
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onhover": {
-	                "enable": true,
-	                "mode": ["bubble", "push"]
-	            },
-	            "onclick": {
-	                "enable": true,
-	                "mode": "push"
-	            }
-	        },
-	        "modes": {
-	            "bubble": {
-	                "distance": 250,
-	                "duration": 2,
-	                "size": 15,
-	                "opacity": 0
-	            },
-	            "repulse": {
-	                "distance": 400,
-	                "duration": 4
-				},
-		
-	        }
-	    }
-	}} />
-
+	<ParticleAnimation />
 	<div className='introduction'>
 		<h2 className='intro-header'> mykol <em>(noun)</em></h2>
 		<i className="fas fa-volume-up fa-2x" id='volume-icon' onClick={soundEffect}></i>
