d220cbbe9807193e09fc476b2e3a49d9b123b0bb
Mykol Benning <mykolbenning@Mykols-MBP.fios-router.home>
2020-04-30 01:01:15 -0400

    home button working, now to add styling and sanimation

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,8 +3,15 @@ import { withRouter } from 'react-router-dom';
 import '../styles/Home.scss';
 import ParticleAnimation from '../particle_animation/ParticleAnimation'
 import NameAudio from '../name audio/NameAudio';
+import { Link, animateScroll as scroll, scroller, Events } from 'react-scroll';
 
 const Home = () => {
+
+
+const scrollToTop = () => {
+	scroller.scrollTo()
+}
+
 
 return(
 	<>
@@ -14,8 +21,18 @@ return(
 		<h2 className='intro-header'> mykol <em>(noun)</em></h2>
 		<NameAudio />
 		<p className='intro-header'>definition: web developer{' '}</p>
+		<button>
+			<Link
+			to='about-section'
+			name='about'
+			spy={true} 
+            smooth={true} 
+            offset={-100} 
+            duration={700} 
+            isDynamic={true} 
+			onClick={() => scrollToTop()}>Test</Link>
+			</button>
 	</div>
-	
     </div>
 		</>
 )}
