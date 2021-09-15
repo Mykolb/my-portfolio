f04c9773ff16f88f2acf5754e4a83cb8d11aeb05
Mykol Benning <mykolbenning@Mykols-MBP.lan>
2021-09-14 22:00:21 -0400

    testing deployment 2

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import { NavLink } from "react-router-dom";
 // import { Link, scroller, Events } from "react-scroll";
-import '../styles/nav.scss';
+import '../styles/Nav.scss';
 import MobileNav from './MobileNav';
 
 
